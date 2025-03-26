bb202e09e547d3c13dd42359990cced9f59ff33d
Branko <banne_nikolic@hotmail.com>
2025-03-26 15:59:59 +0100

    Delete: Tog bort catering från meny så det står boka bara

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <nav className="navbar">
       <a href="/">Startsida</a>            
       <a href="/meny">Meny</a>
-      <a href="/kontakt">Boka catering</a>            
+      <a href="/kontakt">Boka</a>            
     </nav>          
     <img src="/images/Sandys_LOGO.png" alt="Logga" className="logo" />
   </header>
