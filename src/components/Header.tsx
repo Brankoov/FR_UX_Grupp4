f37334c76a6502360b4a3f3ac81e9cf77ac930ee
llovius <you@example.com>
2025-04-03 16:02:04 +0200

    Första version av hamburgermeny som ser förjävlig ut men logiken funkar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
-    <nav className="navbar">
-      <a href="/">Startsida</a>            
-      <a href="/meny">Meny</a>
-      <a href="/kontakt">Boka</a>            
-    </nav>          
-    <img src="/images/Sandys_LOGO.png" alt="Logga" className="logo" />
-  </header>
+      {/* "navbar" visas ej på mobil pga media-query*/}
+      <nav className="navbar">
+        <a href="/">Startsida</a>
+        <a href="/meny">Meny</a>
+        <a href="/kontakt">Boka</a>
+      </nav>
+
+      <div className="logohb">
+        <img src="/images/Sandys_LOGO.png" alt="Logga" className="logo" />
+        {/* Hamburgermeny-knapp (visas endast på mobil) */}
+        <button
+          className="hamburger"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+
+          {isOpen ? "✖" : "☰"}
+        </button>
+      </div>
+      {isOpen ?
+        <nav className="navbar-mobile">
+          <a href="/">Startsida</a><br></br>
+          <a href="/meny">Meny</a><br></br>
+          <a href="/kontakt">Boka</a><br></br>
+        </nav>
+        : ""}
+    </header>
   );
 }
 
