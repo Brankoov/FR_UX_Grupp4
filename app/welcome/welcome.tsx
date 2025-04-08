47f5a4b1bef1e12894ec328fc55ffcbbbc8251af
anis5 <anis5@hotmail.se>
2025-04-08 10:38:48 +0200

    gör om start sidan ändringar i welcome-tsx

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Header from "src/components/Header"
+import Header from "src/components/Header";
 import Footer from "~/components/Footer";
 
 export function Welcome() {
@@ -8,21 +8,34 @@ export function Welcome() {
       <div>
         <Header />
       </div>
-      <section className="hero1">
-        <h1>
-          Upptäck en värld av passion och tradition- två unika smakupplevelser
-        </h1>
-        <div className="main-container">
-          <img
-            src="images/foodtruckbild.JPG"
-            alt="Matbild"
-            className="matbild1-image"
-          />
-          <Link to="/kontakt">
-            <button className="kontakt-button">bokningsförfrågan</button>
-          </Link>
+      <section className="welcome-section">
+        <div className="welcome-container">
+          <div className="welcome-text">
+            <h1>Välkommen!</h1>
+            <h2>Sandys Foodtruck & Catering - Mat med passion!</h2>
+            <p>
+              Här hittar du streetfood med kärlek till råvaror, smaker och
+              människor. Sandra, grundaren bakom Sandys, lagar all mat från
+              grunden med noga utvalda ingredienser och samarbeten med lokala
+              producenter som brödet från bagaren i Göteborg och köttet från
+              kötthandlaren Tina intill.
+              <br />
+              <br />
+              Förvänta dig vällagad, smakrik mat med en personlig touch oavsett
+              om du är ute efter en snabb lunch eller något riktigt gott att ta
+              med.
+            </p>
+            <Link to="/kontakt">
+              <button className="kontakt-button">Bokningsförfrågan</button>
+            </Link>
+          </div>
+          <div className="welcome-image">
+            <img src="/formulärbild.png" alt="Sandra i foodtrucken" />
+          </div>
         </div>
       </section>
+
+      <section className="hero1"></section>
 
       <Footer />
     </main>
