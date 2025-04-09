97bdaeadc22c6f48b143717eb6a7272278cf177c
Branko <banne_nikolic@hotmail.com>
2025-04-09 15:49:46 +0200

    Add: Ändrat i header efter lars kod

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,39 @@
-
+import { useState } from "react";
+ 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
-    <nav className="navbar">
-      <a href="/">Startsida</a>            
-      <a href="/meny">Meny</a>
-      <a href="/kontakt">Boka</a>            
-    </nav>          
-    <img src="/images/Sandys-logo.png" alt="Logga" className="logo" />
-  </header>
+      <div className="logohb">
+        <img src="/images/Sandys-logo.png" alt="Logga" className="logo" />
+ 
+        {/* Visa navbar endast på desktop */}
+        <nav className="navbar">
+          <a href="/">Startsida</a>
+          <a href="/meny">Meny</a>
+          <a href="/kontakt">Boka</a>
+        </nav>
+ 
+        {/* Hamburgermeny endast på mobil */}
+        <button
+          className="hamburger"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? "✖" : "☰"}
+        </button>
+      </div>
+ 
+      {/* Mobilmeny */}
+      {isOpen && (
+        <nav className="navbar-mobile">
+          <a href="/">Startsida</a>
+          <a href="/meny">Meny</a>
+          <a href="/kontakt">Boka</a>
+        </nav>
+      )}
+    </header>
+ 
   );
 }
-
+ 
 export default Header;
