059d83e47100430ff446a5ff5f6c662aac528b4d
llovius <you@example.com>
2025-04-09 09:56:41 +0200

    Fixat formattering

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,32 +4,35 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
-      {/* "navbar" visas ej på mobil pga media-query*/}
-      <nav className="navbar">
-        <a href="/">Startsida</a>
-        <a href="/meny">Meny</a>
-        <a href="/kontakt">Boka</a>
-      </nav>
-
       <div className="logohb">
         <img src="/images/Sandys_LOGO.png" alt="Logga" className="logo" />
-        {/* Hamburgermeny-knapp (visas endast på mobil) */}
+
+        {/* Visa navbar endast på desktop */}
+        <nav className="navbar">
+          <a href="/">Startsida</a>
+          <a href="/meny">Meny</a>
+          <a href="/kontakt">Boka</a>
+        </nav>
+
+        {/* Hamburgermeny endast på mobil */}
         <button
           className="hamburger"
           onClick={() => setIsOpen(!isOpen)}
         >
-
           {isOpen ? "✖" : "☰"}
         </button>
       </div>
-      {isOpen ?
+
+      {/* Mobilmeny */}
+      {isOpen && (
         <nav className="navbar-mobile">
-          <a href="/">Startsida</a><br></br>
-          <a href="/meny">Meny</a><br></br>
-          <a href="/kontakt">Boka</a><br></br>
+          <a href="/">Startsida</a>
+          <a href="/meny">Meny</a>
+          <a href="/kontakt">Boka</a>
         </nav>
-        : ""}
+      )}
     </header>
+
   );
 }
 
